8c6f844df7232624f3daf8c6234111d3e1dfa6b1
Hongpei Zheng <zhenghongpei@gmail.com>
2024-05-26 02:58:00 +0100

    📝 Update documentation

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/Madokami/",
+  base: "/",
 
   lang: "zh-CN",
   title: "Madokami用户指南",
